39cff0923d0dee499b5bef0f9d63bedc0939e77e
Ge Zhenyuan <49439486+yuanluo2@users.noreply.github.com>
2023-02-25 18:24:01 +0800

    Update win_printLastError.c

--- win_printLastError.c
+++ win_printLastError.c
@@ -5,6 +5,7 @@
 #if !defined(__WINDOWS__) && (defined(WIN32) || defined(WIN64) || defined(_MSC_VER) || defined(_WIN32))
 #include <stdio.h>
 #include <windows.h>
+#include <wchar.h>
 
 #define ERROR_MSG_LENGTH_MAX 256
 
@@ -12,14 +13,19 @@ void printLastError(){
 	DWORD errNum = GetLastError();
 	char errMsg[ERROR_MSG_LENGTH_MAX];
 	
-    FormatMessageA(FORMAT_MESSAGE_FROM_SYSTEM,
-					NULL, 
-					errNum,
-					0, 
-					errMsg,
-					ERROR_MSG_LENGTH_MAX, 
-					NULL);
-					
+    DWORD ret = FormatMessageA(FORMAT_MESSAGE_FROM_SYSTEM,
+								NULL, 
+								errNum,
+								0, 
+								errMsg,
+								ERROR_MSG_LENGTH_MAX, 
+								NULL);
+	
+	if (ret == 0){
+		fprintf(stderr, "printLastError() failed, GetLastError() returned: %d.\n", GetLastError());
+		return;
+	}
+	
     printf("errNum: %d => %s", errNum, errMsg);
 }
 
@@ -31,14 +37,19 @@ void printLastErrorW(){
 	DWORD errNum = GetLastError();
 	wchar_t errMsg[ERROR_MSG_LENGTH_MAX];
 	
-    FormatMessageW(FORMAT_MESSAGE_FROM_SYSTEM,
-					NULL, 
-					errNum,
-					0, 
-					errMsg,
-					ERROR_MSG_LENGTH_MAX, 
-					NULL);
-					
+    DWORD ret = FormatMessageW(FORMAT_MESSAGE_FROM_SYSTEM,
+								NULL, 
+								errNum,
+								0, 
+								errMsg,
+								ERROR_MSG_LENGTH_MAX, 
+								NULL);
+	
+	if (ret == 0){
+		fwprintf(stderr, L"printLastError() failed, GetLastError() returned: %d.\n", GetLastError());
+		return;
+	}
+	
     wprintf(L"errNum: %d => %ls", errNum, errMsg);
 }
 
